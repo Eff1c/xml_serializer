0998c09dd3c8c124ae2d9653ca84e9736f8fa93f
eff1c <illia.martyniuk@busmarket.ua>
2023-03-02 22:22:20 +0200

    refactor: test_converter_types. Create ConverterTypeTestCase

--- test/test_converter_types.py
+++ test/test_converter_types.py
@@ -1,108 +1,120 @@
+from typing import NamedTuple
+
 import pytest
 
+from abstract_type import AbstractType
 from xml_serializer.converter_types import String, Integer, Float, Boolean, NestedType
 
-valid_test_data = [
-    (
-        String,
-        "test string",
-        "test string"
-    ),
-    (
-        Integer,
-        "123",
-        123
-    ),
-    (
-        Float,
-        "123.123",
-        123.123
-    ),
-    (
-        Boolean,
-        "true",
-        True
-    ),
-    (
-        Boolean,
-        "false",
-        False
-    ),
-]
+
+class ConverterTypeTestCase(NamedTuple):
+    converter_type: AbstractType
+    payload: str
+    expected_response: any
 
 
-@pytest.mark.parametrize('converter_type, payload, expected_response', valid_test_data)
-def test_common_types(converter_type, payload, expected_response):
-    converter = converter_type()
+class TestConverterTypes:
+    valid_test_data = [
+        ConverterTypeTestCase(
+            converter_type=String,
+            payload="test string",
+            expected_response="test string"
+        ),
+        ConverterTypeTestCase(
+            converter_type=Integer,
+            payload="123",
+            expected_response=123
+        ),
+        ConverterTypeTestCase(
+            converter_type=Float,
+            payload="123.123",
+            expected_response=123.123
+        ),
+        ConverterTypeTestCase(
+            converter_type=Boolean,
+            payload="true",
+            expected_response=True
+        ),
+        ConverterTypeTestCase(
+            converter_type=Boolean,
+            payload="false",
+            expected_response=False
+        ),
+    ]
 
-    assert converter.convert(payload) == expected_response
+    @pytest.mark.parametrize('test_case', valid_test_data)
+    def test_convert(self, test_case: ConverterTypeTestCase):
+        converter = test_case.converter_type()
+
+        assert converter.convert(test_case.payload) == test_case.expected_response
 
 
-correct_schema = {"test": "test"}
-
-
-def correct_data_handling_function():
-    pass
-
-
-incorrect_init_test_data = [
-    (
-        "",
-        correct_data_handling_function,
-        ValueError,
-        "Missing required argument - schema!"
-    ),
-    (
-        None,
-        None,
-        ValueError,
-        "Missing required argument - schema!"
-    ),
-    (
-        {},
-        "",
-        ValueError,
-        "Missing required argument - schema!"
-    ),
-    (
-        correct_schema,
-        "",
-        ValueError,
-        "Missing required argument - data_handling_function!"
-    ),
-    (
-        correct_schema,
-        None,
-        ValueError,
-        "Missing required argument - data_handling_function!"
-    ),
-    (
-        correct_schema,
-        "test",
-        TypeError,
-        "data_handling_function must be callable!"
-    ),
-    (
-        correct_schema,
-        {"test": "test"},
-        TypeError,
-        "data_handling_function must be callable!"
-    )
-]
+class NestedTypeTestCase(NamedTuple):
+    schema: dict
+    data_handling_function: any
+    expected_error: Exception
+    error_message: str
 
 
 class TestNestedType:
-    @pytest.mark.parametrize(
-        'schema, data_handling_function, expected_error, error_message',
-        incorrect_init_test_data
-    )
-    def test_init_with_incorrect_args(
-            self, schema, data_handling_function, expected_error, error_message
-    ):
-        with pytest.raises(expected_error) as error:
-            NestedType(schema, data_handling_function)
+    correct_schema = {"test": "test"}
 
-        assert error.value.__str__() == error_message
+    def correct_data_handling_function():
+        pass
+
+    incorrect_init_test_data = [
+        NestedTypeTestCase(
+            schema="",
+            data_handling_function=correct_data_handling_function,
+            expected_error=ValueError,
+            error_message="Missing required argument - schema!"
+        ),
+        NestedTypeTestCase(
+            schema=None,
+            data_handling_function=None,
+            expected_error=ValueError,
+            error_message="Missing required argument - schema!"
+        ),
+        NestedTypeTestCase(
+            schema={},
+            data_handling_function="",
+            expected_error=ValueError,
+            error_message="Missing required argument - schema!"
+        ),
+        NestedTypeTestCase(
+            schema=correct_schema,
+            data_handling_function="",
+            expected_error=ValueError,
+            error_message="Missing required argument - data_handling_function!"
+        ),
+        NestedTypeTestCase(
+            schema=correct_schema,
+            data_handling_function=None,
+            expected_error=ValueError,
+            error_message="Missing required argument - data_handling_function!"
+        ),
+        NestedTypeTestCase(
+            schema=correct_schema,
+            data_handling_function="test",
+            expected_error=TypeError,
+            error_message="data_handling_function must be callable!"
+        ),
+        NestedTypeTestCase(
+            schema=correct_schema,
+            data_handling_function={"test": "test"},
+            expected_error=TypeError,
+            error_message="data_handling_function must be callable!"
+        ),
+    ]
+
+    @pytest.mark.parametrize(
+        'test_case',
+        incorrect_init_test_data,
+    )
+    def test_init_with_incorrect_args(self, test_case: NestedTypeTestCase):
+        with pytest.raises(test_case.expected_error) as error:
+            NestedType(test_case.schema, test_case.data_handling_function)
+
+        assert error.value.__str__() == test_case.error_message
 
     def test_correct_init(self):
-        NestedType(correct_schema, correct_data_handling_function)
+        NestedType(self.correct_schema, self.correct_data_handling_function)
